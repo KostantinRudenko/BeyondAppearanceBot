0ef92af54713de0766c42d94afee149f001b1a38
KostantinRudenko <rudenkokostya20@gmail.com>
2026-03-27 09:09:17 +0200

    added start reply keyboard markup

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ import asyncio
 import aiogram
 from aiogram import Bot, Dispatcher, html
 from aiogram.types import Message
+from aiogram.types.reply_keyboard_markup import ReplyKeyboardMarkup
+from aiogram.types.reply_keyboard_remove import ReplyKeyboardRemove
+from aiogram.types.keyboard_button import KeyboardButton
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.client.default import DefaultBotProperties
+
+###################
+
+startReplyMarkup = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Create profile"), KeyboardButton(text="Info")]
+            ]
+        )
+
+###################
 
 load_dotenv()
 
@@ -19,11 +32,12 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def startReaction(msg: Message):
-    await msg.answer(f'Hello, {html.bold(msg.from_user.full_name)}')
+    await msg.answer(f'Hello, {html.bold(msg.from_user.full_name)}', reply_markup=startReplyMarkup)
+    logger.info("start command has been received")
 
 async def main():
     logging.basicConfig(filename="main.log", level=logging.INFO)
-    logger.info("Log started")
+    logger.info(f"{"-"*30} Start {"-"*30}")
 
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
